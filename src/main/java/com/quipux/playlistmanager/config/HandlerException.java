107171abbc92e337bd57504dfc7443197f8cdd32
camilohr.dev <chernandez@unimayor.edu.co>
2025-02-08 18:02:29 -0500

    Add handler interceptor to AccessDeniedException

--- src/main/java/com/quipux/playlistmanager/config/HandlerException.java
+++ src/main/java/com/quipux/playlistmanager/config/HandlerException.java
@@ -2,9 +2,12 @@ package com.quipux.playlistmanager.config;
 
 import com.quipux.playlistmanager.common.exceptions.PlayListNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice
@@ -14,6 +17,13 @@ public class HandlerException {
     public ResponseEntity<Object> handlerNotFoundPlayListException(final PlayListNotFoundException e) {
         log.error("HandlerException.handlerNotFoundException {e}", e);
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Object> handleAccessDeniedException(final AccessDeniedException e) {
+        log.error("HandlerException.handleAccessDeniedException {e}", e);
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ExceptionHandler({Exception.class})
