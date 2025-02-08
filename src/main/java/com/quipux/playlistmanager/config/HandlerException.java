18e2cd156a50ecb0e79e40dad85d6661d5e480ba
camilohr.dev <chernandez@unimayor.edu.co>
2025-02-08 17:56:43 -0500

    Add general exception to handler

--- src/main/java/com/quipux/playlistmanager/config/HandlerException.java
+++ src/main/java/com/quipux/playlistmanager/config/HandlerException.java
@@ -3,15 +3,22 @@ package com.quipux.playlistmanager.config;
 import com.quipux.playlistmanager.common.exceptions.PlayListNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
+@ControllerAdvice
 public class HandlerException {
 
     @ExceptionHandler({PlayListNotFoundException.class})
-    public ResponseEntity<Object> handlerNotFoundException(final PlayListNotFoundException e) {
+    public ResponseEntity<Object> handlerNotFoundPlayListException(final PlayListNotFoundException e) {
         log.error("HandlerException.handlerNotFoundException {e}", e);
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handlerException(final Exception e) {
+        log.error("HandlerException.handlerException {e}", e);
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 }
